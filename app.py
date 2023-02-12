94b71117f05352c4b90d87b55ef27f76f367d866
Ekaterina Babarykina <ebabarykina@docdoc.ru>
2023-02-12 16:00:39 +0700

    part 1

---
+++ app.py
@@ -0,0 +1,28 @@
+import telebot
+
+
+TOKEN = '6264296695:AAHrxp72FD8vY4-fQvH5esJgPoGColmtioI'
+
+
+bot = telebot.TeleBot(TOKEN)
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+}
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу, введите команду боту в следующем формате: \n <имя валюты> <в какую валюту перевести> <количество переводимой валюты> \nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+bot.polling()
